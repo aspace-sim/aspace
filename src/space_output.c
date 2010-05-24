1f775a12ccd0fa27dc06725bacdf9a9d0c7df6a0
ray73864 <ray73864@29405c40-0d53-11de-b55b-d397e7115e68>
2010-05-24 09:55:27 +0000

    Fixed up the output so that it shows total power generation, then breaks it down per shield, and also does it in a for loop now so that you don't have to keep updating if you add more shields.
    git-svn-id: https://aspace-sim.googlecode.com/svn/trunk@102 29405c40-0d53-11de-b55b-d397e7115e68

--- src/space_output.c
+++ src/space_output.c
@@ -7,38 +7,36 @@
 
 char *output_shields_raw ()
 {
-	int x;
+	int i;
 	static char buffer[BUFFER_LEN];
-	
-	x = n;
 	
 	/* Allocate 6, 7, 8, 9 and 10 */
 	/* Shield 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 and 12 */
 	
 	/* Master Shield Information */
-	snprintf(buffer, sizeof(buffer), "%d|%f|%f|%d|%3.3f|%d", x, sdb[x].alloc.shields, sdb[x].shield.ratio, sdb[x].shield.maximum, sdb[x].shield.freq, sdb[x].shield.exist);
-	strncat(buffer, "~", sizeof(buffer) - 1);
+	snprintf(buffer, sizeof(buffer), "%f|%f|%f|%d|%3.3f|%d", sdb[n].power.total, sdb[n].alloc.shields, sdb[n].shield.ratio, sdb[n].shield.maximum, sdb[n].shield.freq, sdb[n].shield.exist);
 	
-	/* Fore Shield */
-	
-	strncat(buffer, tprintf("%s|%f|%d|%f", "Fore Shield", sdb[x].alloc.shield[0], sdb[x].shield.active[0], sdb[x].shield.damage[0]), sizeof(buffer) - 1);
-	strncat(buffer, "~", sizeof(buffer) - 1);
-
-	/* Starboard Shield */
-	
-	strncat(buffer, tprintf("%s|%f|%d|%f", "Starboard Shield", sdb[x].alloc.shield[1], sdb[x].shield.active[1], sdb[x].shield.damage[1]), sizeof(buffer) - 1);
-	strncat(buffer, "~", sizeof(buffer) - 1);
-	
-	/* Aft Shield */
-	
-	strncat(buffer, tprintf("%s|%f|%d|%f", "Aft Shield", sdb[x].alloc.shield[2], sdb[x].shield.active[2], sdb[x].shield.damage[2]), sizeof(buffer) - 1);
-	strncat(buffer, "~", sizeof(buffer) - 1);
-	
-	/* Port Shield */
-	
-	strncat(buffer, tprintf("%s|%f|%d|%f", "Port Shield", sdb[x].alloc.shield[3], sdb[x].shield.active[3], sdb[x].shield.damage[3]), sizeof(buffer) - 1);
-	
+	for (i = 0; i < MAX_SHIELD_NAME; ++i) {
+		strncat(
+			buffer, 
+			tprintf("~%s|%f|%f|%d|%f", unparse_shield(i), sdb[n].alloc.shield[i] * sdb[n].power.total, sdb[n].alloc.shield[i], sdb[n].shield.active[i], sdb[n].shield.damage[i]), 
+			sizeof(buffer) - 1
+		);
+	}
+		
 	return buffer;
 }
 
+char *output_beams_raw ()
+{
+
+	static char buffer[BUFFER_LEN];
+	
+	/* Allocate 12 */
+	/* Beam 1, 2, 3, 4, 5 */
+	/* Beam_Active, Beam_Name, Beame_Damage, Beam_Bonus, Beam_Cost, Beam_Range, Beam_Arcs, Beam_Lock, Beam_Load
+	
+	/* Master Beam Information */
+
+}
 /* ------------------------------------------------------------------------ */
