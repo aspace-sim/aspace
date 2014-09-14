3d906c0a93b307762ae7503be40f69074284b64b
Mike Taylor <kovarid@gmail.com>
2014-09-14 13:47:51 -0400

    Update to boder code for logging

--- hdrs/space.h
+++ hdrs/space.h
@@ -662,10 +662,10 @@ extern char* output_shields_raw();
 
 /* from space_border.c - not everything yet */
 extern void free_borderinfo(void *ptr);
-extern void addNewBorder(int border_number, const char* name, double radius, double x, double y, double z, char *buff, char **bp);
-extern void deleteBorder(int border, char *buff, char **bp);
+extern void addNewBorder(dbref executor, int border_number, const char* name, double radius, double x, double y, double z, char *buff, char **bp);
+extern void deleteBorder(dbref executor, int border, char *buff, char **bp);
 extern void list_borders(char *buff, char **bp);
-extern void edit_border(int border_id, const char* setting, const char* new_value, char *buff, char **bp);
+extern void edit_border(dbref executor, int border_id, const char* setting, const char* new_value, char *buff, char **bp);
 extern int get_empire_id (int ship);
 
 /* from space_crypt.c */
